8a1335f2b72e7d83afc45d31294101e2a58559df
donovansmith44 <donovan.smith44@gmail.com>
2022-12-06 19:59:16 -0500

    noticed an error in the code.

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -15,68 +15,65 @@ using namespace std;
         numStatics = 0;
         numVars = 0;
     }
-    void SymbolTable::startSubroutine(string className)
+    void SymbolTable::startSubroutine(string currentClassName)
     {
         numArgs = 0;
         numVars = 0;
         subroutineScopeVariables.clear();
-        subroutineScopeVariables["this"] = make_tuple(className, "argument", 0); //get<0>(tuple) = className
+        subroutineScopeVariables["this"] = make_tuple(currentClassName, "argument", 0);
         numArgs++;
         return;
     }
     void SymbolTable::Define(string name, string type, string kind)
     {
-        switch (kind)
+        if(kind == "STATIC")
         {
-        case "STATIC":
             classScopeVariables[name] = make_tuple(type, kind, numStatics);
             numStatics++;
-            break;
-
-        case "FIELD":
+        }
+        else if(kind == "FIELD")
+        {
             classScopeVariables[name] = make_tuple(type, kind, numFields);
             numFields++;
-            break;
-
-        case "ARG":
+        }
+        else if(kind == "ARG")
+        {
             subroutineScopeVariables[name] = make_tuple(type, kind, numArgs);
             numArgs++;
-            break;
-
-        case "VAR":
+        }
+        else if(kind == "VAR")
+        {
             subroutineScopeVariables[name] = make_tuple(type, kind, numVars);
             numVars++;
-            break;
-
-        default:
-            return;
-            break;
         }
+        else
+        {
+            return;
+        }
+
         return;
     }
     int SymbolTable::VarCount(string kind)
     {
-        switch (kind)
+        if(kind == "STATIC")
         {
-        case "STATIC":
             return numStatics;
-            break;
-
-        case "FIELD":
+        }
+        else if(kind == "FIELD")
+        {
             return numFields;
-            break;
-
-        case "ARG":
+        }
+        else if(kind == "ARG")
+        {
             return numArgs;
-            break;
-
-        case "VAR":
+        }
+        else if(kind == "VAR")
+        {
             return numVars;
-            break;
-
-        default:
+        }
+        else
+        {
             return 0;
-            break;
         }
     }
     string SymbolTable::TypeOf(string name)
